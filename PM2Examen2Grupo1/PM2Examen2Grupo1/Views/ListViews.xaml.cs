e82a672af4ec1694ad53c03372b08561697fc42e
gereld10 <gereldamids42@gmail.com>
2023-07-16 15:49:02 -0600

    ListViews actualizado

---
+++ PM2Examen2Grupo1/PM2Examen2Grupo1/Views/ListViews.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2Examen2Grupo1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViews : ContentPage
+    {
+        public ListViews()
+        {
+            InitializeComponent();
+        }
+    }
+}
