a407a768eb1e03db754df43778cae7092058a086
loida varela <loidavarela@hotmail.es>
2023-07-16 13:45:20 -0600

    PaginaPrincial/2

--- PM2Examen2Grupo1/PM2Examen2Grupo1/Views/PaginaPrincipal.xaml.cs
+++ PM2Examen2Grupo1/PM2Examen2Grupo1/Views/PaginaPrincipal.xaml.cs
@@ -9,12 +9,12 @@ using Xamarin.Forms.Xaml;
 
 namespace PM2Examen2Grupo1.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class PaginaPrincipal : ContentPage
-    {
-        public PaginaPrincipal()
-        {
-            InitializeComponent();
-        }
-    }
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PaginaPrincipal : ContentPage
+	{
+		public PaginaPrincipal ()
+		{
+			InitializeComponent ();
+		}
+	}
 }
