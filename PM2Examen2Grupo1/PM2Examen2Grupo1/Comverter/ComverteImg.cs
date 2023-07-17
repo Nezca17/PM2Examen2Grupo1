a02dea769655580387a21c6441fa5e96b82105d4
loida varela <loidavarela@hotmail.es>
2023-07-16 22:47:30 -0600

    prueba de firma

---
+++ PM2Examen2Grupo1/PM2Examen2Grupo1/Comverter/ComverteImg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PM2Examen2Grupo1.Comverter
+{
+    public class ByteArrayToImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ImageSource retSource = null;
+            if (value != null)
+            {
+                byte[] imageAsBytes = (byte[])value;
+                retSource = ImageSource.FromStream(() => new System.IO.MemoryStream(imageAsBytes));
+            }
+            return retSource;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PM2Examen2Grupo1/PM2Examen2Grupo1/Views/PaginaPrincipal.xaml.cs
+++ PM2Examen2Grupo1/PM2Examen2Grupo1/Views/PaginaPrincipal.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Plugin.Media.Abstractions;
+using SignaturePad.Forms;
+using System;
 using System.Collections.Generic;
+using Plugin.Media;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +16,32 @@ namespace PM2Examen2Grupo1.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PaginaPrincipal : ContentPage
 	{
+        Plugin.Media.Abstractions.MediaFile firma = null;
 		public PaginaPrincipal ()
 		{
 			InitializeComponent ();
 		}
 
-        private void btnfirmar_Clicked(object sender, EventArgs e)
-        {
+       // public String Getimage64() 
+        //{
+           // if (firma != null)
+            //{ 
+              //using (MemoryStream memory = new MemoryStream))
+               // {
+                  //  Stream stream = firma.GetStream();
+                    //Stream.CopyTo(memory);
+                    //byte[] fotobyte = memory.ToArray();
 
+                  //  String Base64 = Convert.ToBase64String(fotobyte);
+                  //  return Base64;
+               // }
+            //}
+           // return null;
+       // }
+
+        private async void btnfirmar_Clicked(object sender, EventArgs e)
+        {
+            Stream image = await PadView.GetImageStreamAsync(SignatureImageFormat.Jpeg);
         }
 
         private void grabarvoz_Clicked(object sender, EventArgs e)
@@ -34,6 +56,7 @@ namespace PM2Examen2Grupo1.Views
 
         private void btnsalvar_Clicked(object sender, EventArgs e)
         {
+
 
         }
 
